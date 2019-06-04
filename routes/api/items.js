0de045acf94508022098866e9fd07f7e8a99775c
Kent <kentfero@gmail.com>
2019-06-05 06:25:43 +0800

    Create endpoint to update item.

--- routes/api/items.js
+++ routes/api/items.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const auth = require('../../middleware/auth');
+const auth = require("../../middleware/auth");
 
 // Item Model
-const Item = require('../../models/Item');
+const Item = require("../../models/Item");
 
 // @route  GET api/items
 // @desc   Get all items
 // @access Public
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   Item.find()
     .sort({ date: -1 })
     .then(items => res.json(items));
@@ -17,7 +17,7 @@ router.get('/', (req, res) => {
 // @route  POST api/items
 // @desc   Create an item
 // @access Private
-router.post('/', auth, (req, res) => {
+router.post("/", auth, (req, res) => {
   const newItem = new Item({
     name: req.body.name
   });
@@ -25,10 +25,33 @@ router.post('/', auth, (req, res) => {
   newItem.save().then(item => res.json(item));
 });
 
+// @route  PUT api/items
+// @desc   Update an item
+// @access Private
+router.put("/:id", auth, (req, res) => {
+  const itemID = req.params.id;
+  const itemInputs = req.body;
+
+  Item.findById(itemID, function(err, item) {
+    if (!item) res.status(404).json({ success: false, msg: "Item not found." });
+    else {
+      item.name = itemInputs.name;
+      item
+        .save()
+        .then(item => res.json(item))
+        .catch(err =>
+          res
+            .status(400)
+            .json({ success: false, msg: `Update failed please. ${err}` })
+        );
+    }
+  });
+});
+
 // @route  DELETE api/items/:id
 // @desc   Delete an item
 // @access Private
-router.delete('/:id', auth, (req, res) => {
+router.delete("/:id", auth, (req, res) => {
   Item.findById(req.params.id)
     .then(item => item.remove().then(() => res.json({ success: true })))
     .catch(err => res.status(404).json({ success: false }));
